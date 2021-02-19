55d02b9961d0525cc422088034277f5feb31ea73
pc <pc@DESKTOP-VAGMEPN>
2021-02-19 14:59:53 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " +customer.Surname +"Added to Database");
+        }
+        public void List(Customer[] customers )
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name + " " + customer.Surname + "Listed");
+            }
+            
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + "Deleted");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer
+            {
+                Id = 1,
+                Name = "Mike",
+                Surname = "Sobbarrow",
+                IdentityNumber = "12345678901"
+            };
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                Name = "Burak",
+                Surname = "Gaytan",
+                IdentityNumber = "9876543212"
+            };
+            Customer[] customerListe = new Customer[] { customer1, customer2 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+
+            customerManager.Add(customer2);
+            customerManager.Delete(customer2);
+
+            customerManager.List(customerListe);
+
+            Console.ReadKey();
+        }
+    }
+}
